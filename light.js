9854afcb7cdf1a096999e7122f68890cbefeb711
jrgleason <jackie@jackie-Latitude-E6410.(none)>
2012-05-10 22:51:40 -0400

    Removing comments

--- light.js
+++ light.js
@@ -5,7 +5,5 @@ exports.toggle = function(req, res){
 }
 exports.explain = function(req, res){
   res.send('Please use only /toggle/lcd');
-  // not sure why you were invoking addon.hello() here, but
-  // you could still do that, no need for the return either
 }
 
